172bd38ae7a14d56d02c75cb90084c7a520c393b
Kabilan C <85838280+kabilankannu@users.noreply.github.com>
2023-02-22 10:23:52 +0530

    Update hper.js

--- src/hper.js
+++ src/hper.js
@@ -20,7 +20,7 @@ class Hper extends React.Component{
         this.setState({c:"5"});
     }
     changeAll=()=>{
-        this.setState({a:"5",b:"2",c:"6"});
+        this.setState({a:"1",b:"2",c:"6"});
 
     }
 
@@ -31,6 +31,8 @@ class Hper extends React.Component{
        <button onClick={this.changeB}>Change B</button>
        <button onClick={this.changeC}>Change c</button>
        <button onClick={this.changeAll}>Change All</button>
+       <h6>a:  {this.state.a}   b:  {this.state.b}   c:  {this.state.c}</h6>
+       <h5>Formula : (a³+b³+c³)+3[(a+b+c)(ab+ac+bc)-abc]</h5>
        <p>{ (((this.state.a)**3)+((this.state.b)**3)+((this.state.c)**3))
         +((this.state.a+this.state.b+this.state.c)*((this.state.a)*this.state.b+((this.state.a)*(this.state.c))+((this.state.b)*(this.state.c)))-((this.state.a)*(this.state.b)*(this.state.c)))
         } </p>
